99916656a1936698aa9e9c30a20d568f3fd5857a
Usama Bin Haider <36899171+usamafast@users.noreply.github.com>
2020-12-23 21:52:31 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+firebase.initializeApp({
+  apiKey: "AIzaSyAy1eakhEzpr7SoFNkZobMvf2oG7NuHvd0",
+  authDomain: "authentication-30583.firebaseapp.com"
+})
+
+class App extends Component {
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      ],
+    
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => { 
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+          <span>
+            <div>You are successfully signed in</div>
+            <button onClick={() => firebase.auth().signOut()} className = "App-header">Sign out!</button>
+            <h1>Hello {firebase.auth().currentUser.displayName}</h1>
+            
+          </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/fire.js
@@ -0,0 +1,10 @@
+import firebase from "firebase"
+
+
+
+const fire = firebase.initializeApp({
+    apiKey: "AIzaSyAy1eakhEzpr7SoFNkZobMvf2oG7NuHvd0",
+    authDomain: "authentication-30583.firebaseapp.com"
+  })
+
+  export default fire;
